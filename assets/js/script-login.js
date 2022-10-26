1abf95817a335aa1abd2433da8475319abed0dbd
angelomarcio40 <101677257+angelomarcio40@users.noreply.github.com>
2022-10-26 20:34:21 -0300

    css corrigido

---
+++ assets/js/script-login.js
@@ -0,0 +1,10 @@
+// ES6+
+const validalogin = () =>{
+    // cria var email
+    let email = $('#email').val()
+
+    // exibe mensagem no console do navegador
+    // usado para debug da aplicação
+    // verificação de erros
+    console.log(`Email:`+email)
+}
